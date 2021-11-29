043e0421b18580029ac268824a65c2b537d1753a
TimE0is0ReaL <mr.mahahei.sasha@gmail.com>
2021-11-29 21:20:09 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CheckBox from './components/emoji-states';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CheckBox />
   </React.StrictMode>,
   document.getElementById('root')
 );
