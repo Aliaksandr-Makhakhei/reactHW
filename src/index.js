6a65e3cc11729559d6330509f04ddb3e09bad78e
Sasha <mr.mahahei.sasha@gmail.com>
2021-12-01 13:26:21 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CyclicButton from './component/Cyclic';
+import CyclicButton from './component/cyclicButton/';
 
 ReactDOM.render(
   <React.StrictMode>
