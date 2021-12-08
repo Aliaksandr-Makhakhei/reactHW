46fea0b7118eb085a05efef0b043382e1a56ae59
Sasha <mr.mahahei.sasha@gmail.com>
2021-12-08 22:59:41 +0300

    finish task

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,33 +1,44 @@
 import React, { useState, useEffect } from "react";
-import './style.css'
+import "./style.css";
 
-const pagesNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+const pagesNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 const Pagination = () => {
+  const [pageNumber, setPageNumber] = useState(0);
+  const [posts, setPosts] = useState([]);
 
-    const [pageNumber, setPageNumber] = useState(0);
-    const [posts, setPosts] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_page=${pageNumber}&_limit=10`);
+      const data = await response.json();
+      setPosts(data);
+    }
+    fetchData();
+  }, [pageNumber]);
 
-    useEffect(async () => {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_page=${pageNumber}&_limit=10`)
-            const data = await response.json()
-            setPosts(data) 
-    }, [pageNumber])
-
-    const buttonList = pagesNumbers.map(number => {
-        return <button onClick = {() => setPageNumber(number)} key = {number}>{number}</button>
-    })
-
-    const imageList = posts.map(post => {
-        return <img key={post.id} src={post.url}></img>
-    })
-
+  const buttonsList = pagesNumbers.map((number) => {
     return (
-        <>
-        {imageList}
-        {buttonList}
-        </>
-    )
-}
+      <button onClick={() => setPageNumber(number)} key={number}>
+        {number}
+      </button>
+    );
+  });
 
-export default Pagination
+  const post = posts.map((post) => {
+    return (
+      <div key={post.id} className="post">
+        <img key={post.url} src={post.url} alt={post.title}></img>
+        <p key={post.title}>{post.title}</p>
+      </div>
+    );
+  });
+
+  return (
+    <div className="wrapper">
+      {post}
+      <div>{buttonsList}</div>
+    </div>
+  );
+};
+
+export default Pagination;
