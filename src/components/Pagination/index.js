29666926575e89605d9a5503ae6a5a40af82a9db
TimE0is0ReaL <mr.mahahei.sasha@gmail.com>
2021-12-08 19:25:14 +0300

    add solution

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -6,19 +6,25 @@ const pagesNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 const Pagination = () => {
 
     const [pageNumber, setPageNumber] = useState(0);
+    const [posts, setPosts] = useState([])
 
-    useEffect(() => {
-            fetch(`https://jsonplaceholder.typicode.com/photos?_page=${pageNumber}&_limit=10`)
-            .then(response => response.json())
-            .then(response => console.log(response))
+    useEffect(async () => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_page=${pageNumber}&_limit=10`)
+            const data = await response.json()
+            setPosts(data) 
     }, [pageNumber])
 
     const buttonList = pagesNumbers.map(number => {
-        return <button onClick = {setPageNumber(number)} key = {number}>{number}</button>
+        return <button onClick = {() => setPageNumber(number)} key = {number}>{number}</button>
+    })
+
+    const imageList = posts.map(post => {
+        return <img key={post.id} src={post.url}></img>
     })
 
     return (
         <>
+        {imageList}
         {buttonList}
         </>
     )
