c1481554b686c52baa1c8232357e5df450f5c1c2
Sasha <mr.mahahei.sasha@gmail.com>
2021-12-21 17:15:41 +0300

    add solution

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,25 +2,32 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./style.css";
 
+const types = [1, 2];
+const colors = ["red", "gray", "blue", "cyan", "yellow", "orange"];
+
 const NavBar = () => {
-  const [type, setType] = useState("");
-  const [color, setColor] = useState("");
+  const [type, setType] = useState("1");
+  const [color, setColor] = useState("red");
   const history = useHistory();
 
-  history.push(`/figure/${type}/${color}`);
-
-  const types = [1, 2];
-  const colors = ["red", "gray", "blue", "cyan", "yellow", "orange"];
+  const onClickType = (type) => {
+    setType(type);
+    history.push(`/figure/${type}/${color}`);
+  };
+  const onClickColor = (color) => {
+    setColor(color);
+    history.push(`/figure/${type}/${color}`);
+  };
 
   const typeBtn = types.map((type) => {
     return (
-      <button key={type} onClick={() => setType(type)} className="choose__figure">{type}</button>
+      <button key={type} onClick={() => onClickType(type)} className="choose__figure">{type}</button>
     );
   });
 
   const figureColor = colors.map((color) => {
     return (
-      <button key={color} onClick={() => setColor(color)} className="choose__color">{color}</button>
+      <button key={color} onClick={() => onClickColor(color)} className="choose__color">{color}</button>
     );
   });
 
